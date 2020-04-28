b2289183813993891aaf38df01796172e009e23f
Meiert Grootes <m.grootes@esciencecenter.nl>
2020-04-29 00:44:38 +0200

    fixed typo in buildeeco

--- Docker/buildeeco.sh
+++ Docker/buildeeco.sh
@@ -2,5 +2,5 @@
 
 curDate=$(date +%d%m%Y)
 
-docker build --file eecoDockerfile --network=host -t nlesc/mlokum:${curDate} -t nlesc/eecolokum:latest .
+docker build --file eecoDockerfile --network=host -t nlesc/eecolokum:${curDate} -t nlesc/eecolokum:latest .
 #docker push nlesc/lokum:$curDate
